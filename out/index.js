97c6e27bc945bd765dd8aa98df14752cb045ee4e
PatrickTey <93598645+PatrickTey@users.noreply.github.com>
2022-06-16 17:54:47 +0000

    first

---
+++ out/index.js
@@ -0,0 +1,5 @@
+async function fetchUsers() {
+    const userIDs = await fetch('/users');
+    const users = await Promise.all(userIDs.map(userID => fetch(`/users/${userID}`)));
+    return users;
+  }
